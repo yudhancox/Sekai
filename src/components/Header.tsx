b90e053dc9bc40f88e3832fe1b0ae666368c599c
yudhancox <57454500+yudhancox@users.noreply.github.com>
2026-04-30 11:25:05 +0700

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -113,7 +113,7 @@ export function Header() {
               <Play className="w-5 h-5 text-white fill-white" />
             </div>
             <span className="font-display font-bold text-xl gradient-text">
-              SekaiDrama
+              telegram @dramachina
             </span>
           </Link>
 
